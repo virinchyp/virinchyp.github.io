78186c4e132c72fd4b80fb8fc72cfdf89cd96d9c
virinchy.panangipalli <virinchyp@gmail.com>
2020-10-17 18:26:32 +1100

    Anchor template

--- sap.uxap.sample.AnchorBarWithNumbers/Component.js
+++ sap.uxap.sample.AnchorBarWithNumbers/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
 	"use strict";
 
-	return UIComponent.extend("sap.uxap.sample.ObjectPageState.Component", {
+	return UIComponent.extend("sap.uxap.sample.AnchorBarWithNumbers.Component", {
 		metadata: {
 			manifest: "json"
 		}
